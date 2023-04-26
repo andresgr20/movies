225bf3ac6f05a72fa3735deae47c1168228b27da
Andres <andresgr2094@hotmail.com>
2023-04-26 19:48:25 -0400

    git frontend

---
+++ Client/client/src/components/reviews/Reviews.tsx
@@ -0,0 +1,14 @@
+import { useEffect,useRef } from "react";
+import api from '../../api/axiosConfig';
+
+
+const Reviews = () => {
+    const revText = useRef();
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Reviews;
